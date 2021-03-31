270eed970696d31f1028430103710785ff5c62e4
Khadi431 <khadijahharuna22@gmail.com>
2021-03-31 08:27:16 -0700

    added files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import Main from './components/Main';
+import Students from './components/Students';
+
+function App() {
+  return (
+    <div>
+      <Main/>
+      <Students/>
+    </div>
+  );
+}
+
+export default App;
